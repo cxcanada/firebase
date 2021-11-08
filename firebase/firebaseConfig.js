6aac43c369dfec3c86c78fec62fbbffbd7288e6f
Xiao Chen <cxcanada@hotmail.com>
2021-11-08 09:27:07 -0700

    firebase setup

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDH4SWryMC0VEIzJVtTGOUvVGMwUnYjbQc",
+    authDomain: "dmit2008-oa01.firebaseapp.com",
+    databaseURL: "https://dmit2008-oa01-default-rtdb.firebaseio.com",
+    projectId: "dmit2008-oa01",
+    storageBucket: "dmit2008-oa01.appspot.com",
+    messagingSenderId: "1036115790551",
+    appId: "1:1036115790551:web:a61033ce89d056e3317e92",
+    measurementId: "G-7FQ0XQ35Z8"
+};
+
+export default firebaseConfig;
---
+++ firebase/index.js
@@ -0,0 +1,7 @@
+// Build the APP Instance of Firebase
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase
